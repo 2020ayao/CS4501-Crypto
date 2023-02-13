7140d87567942815c87907657460d4f3218e7324
Adam Yao <adamyao3240@gmail.com>
2023-02-13 18:55:41 -0500

    Changes for hw3

---
+++ Homework 3/btcscript.py
@@ -0,0 +1,168 @@
+import sys
+error_lookup = {
+    1 : "Invalid magic number",
+    2 : "Invalid header version",
+    3 : "Invalid previous header hash",
+    4 : "Invalid timestamp",
+    5 : "Invalid transaction version",
+    6 : "The Merkle tree hash in the header is incorrect"
+}
+fileName = sys.argv[1]
+
+
+
+def readFile(numBytes):
+    with open(fileName, mode='rb') as file: # b is important -> binary
+        return file.read(numBytes)
+
+def compactSize():
+    b = readFile(1) # read first byte
+    dec = int.from_bytes(b)
+    if dec >= 0 and dec < 253:
+        return dec
+    elif dec == 253:
+        return int.from_bytes(readFile(2), 'little')
+    elif dec == 254:
+        return int.from_bytes(readFile(4), 'little')
+    else:
+        return int.from_bytes(readFile(8), 'little')
+
+
+
+class Preamble:
+    def __init__(self):
+        self.magic_number = None
+        self.size = None
+    def getMagicNumber(self):
+        bytes = readFile(4)
+        self.magic_number = int.from_bytes(bytes, 'little')
+    def getSize(self):
+        bytes = readFile(4)
+        self.size = int.from_bytes(bytes, 'little')
+
+
+class Block:
+    def __init__(self):
+        self.preamble = None
+        self.header = None
+        self.txn_count = None
+        self.transactions = None
+
+    def getPreamble(self):
+        self.preamble = Preamble()
+        self.preamble.getMagicNumber()
+        self.preamble.getSize()
+    def getHeader(self):
+        self.header = Header()
+        self.header.getVersion()
+        self.header.getPrevHash()
+    def getTransactions(self):
+        self.transactions = Transaction()
+        self.transactions.getTXInput_Count()
+
+class Header: # header class 
+    def __init__(self):
+        self.version = None
+        self.prev_hash = None
+        self.merkle_root_hash = None
+        self.time = None
+        self.nBits = None
+        self.nonce = None
+    def getVersion(self):
+        bytes = readFile(4)
+        self.version = int.from_bytes(bytes, 'little')
+    def getPrevHash(self):
+        bytes_array = bytearray(readFile(32))
+        bytes_array[::-1] # reverse the endianess of these bytes
+        self.prev_hash = b''.join([bytes_array])
+    def getMerkleRootHash(self):
+        bytes_array = bytearray(readFile(32))
+        self.merkle_root_hash = b''.join([bytes_array])
+    def getTime(self):
+        bytes = readFile(4)
+        self.time = int.from_bytes(bytes, 'little')
+    def nBits(self):
+        bytes = readFile(4)
+        self.nBits = int.from_bytes(bytes, 'little')
+    def getNonce(self):
+        bytes = readFile(4)
+        self.nonce = int.from_bytes(bytes, 'little')
+
+class TransactionInput:
+    def __init__(self):
+        self.hash = None
+        self.index = None
+        self.in_script_bytes = None
+        self.signature_script = None
+        self.sequence = None
+
+    def getHash(self):
+        self.hash = int.from_bytes(readFile(32),'little')
+    def getIndex(self):
+        self.hash = int.from_bytes(readFile(4),'little')
+    def getInScriptBytes(self):
+        self.in_script_bytes = compactSize()
+        print("in_script_bytes: ", self.in_script_bytes)
+    def getSignatureScript(self):
+        self.signature_script = int.from_bytes(readFile(self.in_script_bytes),'little') # is this in decimal or hex?
+    def getSequence(self):
+        self.sequence = int.from_bytes(readFile(4))
+
+
+class Transaction:
+    def __init__(self):
+        self.version = None
+        self.tx_in_count = None
+        self.tx_in = None
+        self.tx_out_count = None
+        self.tx_out = None
+        self.lock_time = None
+
+    def getVersion(self):
+        self.version = int.from_bytes(readFile(4), 'little')
+    
+    def getTXInput_Count(self):
+        self.tx_in_count = compactSize()
+
+    # def getTransactionInput(self):
+        
+    
+        
+
+    def getTXInputs(self):
+        self.tx_in = []
+        for _ in range(self.tx_in_count):
+            tr = TransactionInput()
+            tr.getHash()
+            tr.getIndex()
+            tr.getInScriptBytes()
+            tr.getSignatureScript()
+            tr.getSequence()
+            tr.getTransactionInput()
+        
+            self.tx_in.append(tr)
+    
+    def getLockTime(self):
+        self.lock_time = int.from_bytes(readFile(4), 'little')
+
+
+
+
+bl = Block()
+bl.getPreamble()
+
+bl.getHeader()
+bl.header.getPrevHash()
+
+bl.getTransactions()
+bl.transactions.getTXInput_Count()
+print("tx_in_count: ", bl.transactions.tx_in_count)
+# print("prev_hash", bl.header.prev_hash)
+
+
+
+
+
+
+
+
--- Homework 3/change_byte.py
+++ Homework 3/change_byte.py
@@ -5,3 +5,6 @@ assert (len(sys.argv)==3)
 data = bytearray(sys.stdin.buffer.read())
 data[int(sys.argv[1])] = int(sys.argv[2])
 sys.stdout.buffer.write(bytes(data))
+
+
+
